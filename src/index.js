baf110727246e632f086327f2d0600a2fd08ab84
jordanrath <rath.jordan@icloud.com>
2022-11-01 13:42:21 -0600

    first commit

---
+++ src/index.js
@@ -0,0 +1,167 @@
+import FetchWrapper from "./fetch-wrapper.js";
+import { capitalize, calculateCalories } from "./helpers.js";
+import snackbar from "snackbar";
+import AppData from "./app-data.js";
+import "snackbar/dist/snackbar.min.css";
+import { API } from "./store2api.js"; 
+
+import Chart from "chart.js/auto";
+
+const appData = new AppData();
+
+const list = document.querySelector("#food-list");
+const form = document.querySelector("#create-form");
+const name = document.querySelector("#create-name");
+const carbs = document.querySelector("#create-carbs");
+const protein = document.querySelector("#create-protein");
+const fat = document.querySelector("#create-fat");
+
+const registerCloseBtns = () => {
+  const cardClosers = document.querySelectorAll(".delete-btn");
+  cardClosers.forEach((card, index) => {
+    if (card.oldOnClick) {
+      card.removeEventListener('click', card.oldOnClick)
+    }
+
+    const newOnClick = () => {
+      handleCardClose(index, card);
+    };
+
+    card.addEventListener('click', newOnClick)
+    card.oldOnClick = newOnClick;
+  })
+}
+
+const handleCardClose = (position, card) => {
+  API.post("/close", {
+    position
+  }).then((data) => {
+    if (data.error) {
+      console.error(data.error)
+      snackbar.show("Unable to remove item.")
+    } else {
+      card.parentNode.parentNode.remove();
+      snackbar.show("Removed item.");
+      appData.removeFood(position);
+      render();
+    }
+  });
+}
+
+const displayEntry = (name, carbs, protein, fat) => {
+  appData.addFood(carbs, protein, fat);
+  list.insertAdjacentHTML(
+    "beforeend",
+    `<li class="card">
+        <div>
+          <h3 class="name">${capitalize(name)}</h3>
+          <button type="button" class="delete-btn">Close</button>
+          <div class="calories">${calculateCalories(
+            carbs,
+            protein,
+            fat
+          )} calories</div>
+          <ul class="macros">
+            <li class="carbs"><div>Carbs</div><div class="value">${carbs}g</div></li>
+            <li class="protein"><div>Protein</div><div class="value">${protein}g</div></li>
+            <li class="fat"><div>Fat</div><div class="value">${fat}g</div></li>
+          </ul>
+        </div>
+      </li>`
+  );
+};
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  API.post("/", {
+    fields: {
+      name: { stringValue: name.value },
+      carbs: { integerValue: carbs.value },
+      protein: { integerValue: protein.value },
+      fat: { integerValue: fat.value },
+    },
+  }).then((data) => {
+    console.log(data);
+    if (data.error) {
+      // there was an error
+      snackbar.show("Some data is missing.");
+      return;
+    }
+
+    snackbar.show("Food added successfully.");
+
+    displayEntry(name.value, carbs.value, protein.value, fat.value);
+    render();
+
+    name.value = "";
+    carbs.value = "";
+    protein.value = "";
+    fat.value = "";
+  });
+});
+
+const init = () => {
+  API.get("/").then((data) => {
+    data.forEach((item) => {
+
+      displayEntry(
+        item.name,
+        item.carbs,
+        item.protein,
+        item.fat
+      );
+    });
+    render();
+  });
+};
+
+let chartInstance = null;
+const renderChart = () => {
+  chartInstance?.destroy();
+  const context = document.querySelector("#app-chart").getContext("2d");
+
+  chartInstance = new Chart(context, {
+    type: "bar",
+    data: {
+      labels: ["Carbs", "Protein", "Fat"],
+      datasets: [
+        {
+          label: "Macronutrients",
+          data: [
+            appData.getTotalCarbs(),
+            appData.getTotalProtein(),
+            appData.getTotalFat(),
+          ],
+          backgroundColor: ["#25AEEE", "#FECD52", "#57D269"],
+          borderWidth: 3, // example of other customization
+        },
+      ],
+    },
+    options: {
+      scales: {
+        yAxes: [
+          {
+            ticks: {
+              beginAtZero: true,
+            },
+          },
+        ],
+      },
+    },
+  });
+};
+
+const totalCalories = document.querySelector("#total-calories");
+
+const updateTotalCalories = () => {
+  totalCalories.textContent = appData.getTotalCalories();
+};
+
+const render = () => {
+  renderChart();
+  updateTotalCalories();
+  registerCloseBtns();
+};
+
+init();
---
+++ src/store2api.js
@@ -0,0 +1,84 @@
+import store from 'store2';
+
+const create404Response = async () => {
+    return {
+        error: "Could not find the service requested"
+    }
+}
+
+const createFoodItem = async (data) => {
+    const { fields = {} } = data;
+    const { name: nameRaw = {}, carbs: carbsRaw = {}, protein: proteinRaw = {}, fat: fatRaw = {} } = fields;
+    const { stringValue: name = null } = nameRaw;
+    const { integerValue: carbsStr = 0 } = carbsRaw;
+    const { integerValue: proteinStr = 0 } = proteinRaw;
+    const { integerValue: fatStr = 0 } = fatRaw;
+    const carbs = parseInt(carbsStr);
+    const protein = parseInt(proteinStr);
+    const fat = parseInt(fatStr);
+    //null check on name
+    if (typeof name != 'string') {
+        return {
+            error: "Name is a required field and cannot be null"
+        }
+    }
+
+    if (typeof carbs != 'number' || typeof protein != 'number' || typeof fat != 'number') {
+        return {
+            error: "Nutritional Info (carbs, protein, fat) must be numbers"
+        }
+    }
+
+    const newFood = {
+        name: name, 
+        carbs: carbs, 
+        protein: protein, 
+        fat: fat,
+    };
+    let currentFood = store.get('food-items');
+    currentFood = (currentFood instanceof Array ? currentFood : []);
+    currentFood.push(newFood);
+    await setFoodItems(currentFood);
+    return {
+        newCount: currentFood.length,
+        newItem: newFood,
+    }
+}
+    const setFoodItems = async (items) => {
+        store.set("food-items", items, true);
+    }
+
+    const getFoodItems = async (data) => {
+        let currentFood = store.get('food-items');
+        currentFood = (currentFood instanceof Array ? currentFood : []);
+        return currentFood;
+    }
+
+    const removeFoodItem = async (data) => {
+        const { position = -1 } = data;
+        const curFood = await getFoodItems();
+        if (curFood.length > position && position >= 0) {
+            curFood.splice(position, 1);
+            await setFoodItems(curFood);
+            return {};
+        }
+        return {
+            error: "Unable to find an item to remove."
+        }
+    }
+
+export const API = {
+    post: async (path, data) => {
+        switch(path) {
+            case "/": return createFoodItem(data);
+            case "/close": return removeFoodItem(data);
+            default: return create404Response();
+        }
+    },
+    get: async (path, data) => {
+        switch(path) {
+            case "/": return getFoodItems(data);
+            default: return create404Response();
+        }
+    },
+}
