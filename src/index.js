baadefbc52f6953a5ad8ab327a4f93f281d40da6
jordanrath <rath.jordan@icloud.com>
2022-11-15 14:34:46 -0700

    typeerror fix

--- src/index.js
+++ src/index.js
@@ -5,10 +5,12 @@ import AppData from "./app-data.js";
 import { API } from "./store2api.js";
 // import '../node-snackbar/src/sass/snackbar.sass'
 import "node-snackbar/dist/snackbar.min.css";
-// import "snackbar/dist/snackbar.min.css"; 
+//import "snackbar/dist/snackbar.min.css"; 
 
 import Chart from "chart.js/auto";
 import snackbar from "node-snackbar";
+// import "node-snackbar.css/"
+// import snackbar from "node-snackbar";
 // import "node-snackbar/dist/snackbar.min.css";
 // import snackbar from "node-snackbar/src/js/snackbar";
 
