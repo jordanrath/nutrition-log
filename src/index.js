9788f24f74aa439b005d2c17466048d7dcd3b010
jordanrath <rath.jordan@icloud.com>
2022-11-15 14:37:04 -0700

    accessing .../node_modules

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import "node-snackbar/dist/snackbar.min.css";
 //import "snackbar/dist/snackbar.min.css"; 
 
 import Chart from "chart.js/auto";
-import snackbar from "node-snackbar";
+import snackbar from "../node_modules/node-snackbar";
 // import "node-snackbar.css/"
 // import snackbar from "node-snackbar";
 // import "node-snackbar/dist/snackbar.min.css";
