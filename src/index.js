1b0a8d38d96a1cc0d42b4482001045c8c36bb118
jordanrath <rath.jordan@icloud.com>
2022-11-15 14:42:22 -0700

    node_modules css

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { capitalize, calculateCalories } from "./helpers.js";
 import AppData from "./app-data.js";
 import { API } from "./store2api.js";
 // import '../node-snackbar/src/sass/snackbar.sass'
-import "../node_modules/node-snackbar/dist/snackbar.min.css";
+import "../../node_modules/node-snackbar/dist/snackbar.min.css";
 //import "snackbar/dist/snackbar.min.css"; 
 
 import Chart from "chart.js/auto";
