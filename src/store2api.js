2b3873e632adeb4d929e150e3cc3e2ad048b71fb
jordanrath <rath.jordan@icloud.com>
2022-12-08 17:44:13 -0700

    export API

--- src/store2api.js
+++ src/store2api.js
@@ -67,7 +67,7 @@ const createFoodItem = async (data) => {
         }
     }
 
-export default API = {
+const API = {
     post: async (path, data) => {
         switch(path) {
             case "/": return createFoodItem(data);
@@ -82,3 +82,5 @@ export default API = {
         }
     },
 }
+
+export default API;
