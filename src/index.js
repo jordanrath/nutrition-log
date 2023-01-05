046b071559cc111f590a60d6c3fe4f4385d04b64
jordanrath <rath.jordan@icloud.com>
2023-01-04 23:53:13 -0700

    chart import

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { API } from "./store2api.js";
 // import '../node-snackbar/src/sass/snackbar.sass'
 //import "snackbar/dist/snackbar.min.css"; 
 
-import Chart from "/chart.js/auto/auto.js";
+import Chart from "/chart.js/auto/";
 //import "../node_modules/node-snackbar/dist/snackbar.min.css";
 //import snackbar from "node-snackbar"
 // import snackbar from "node-snackbar";
